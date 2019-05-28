d8d00402b60b21c98df6fb8131b1b6cd5e8f90a6
Peter Graham <peter.graham@15gifts.com>
2019-05-28 16:09:03 +0100

    adding featured images to blogposts

--- src/components/blog-card.js
+++ src/components/blog-card.js
@@ -1,15 +1,19 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 const styles = `
   margin-bottom:1rem;
 
-  .card-box {
-    border-left: 5px solid #76BA8F;
+  .card-wrapper {
     -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
     -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
     box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+  }
+
+  .card-content {
+    border-left: 5px solid #76BA8F;
     padding: 1rem 0.5rem 0.1rem 0.5rem;
   }
 
@@ -31,10 +35,13 @@ const BlogCard = ({post}) => {
   return (
     <div key={post.id} css={css(styles)}>
       <Link to={post.fields.slug}>
-        <div className='card-box'>
-          <h3>{post.frontmatter.title}</h3>
-          <small className='light'>{post.frontmatter.date}</small>
-          <p><i>{post.excerpt}</i></p>
+        <div className='card-wrapper'>
+          <Img sizes={post.frontmatter.featuredImage.childImageSharp.sizes} />
+          <div className='card-content'>
+            <h3>{post.frontmatter.title}</h3>
+            <small className='light'>{post.frontmatter.date}</small>
+            <p><i>{post.excerpt}</i></p>
+          </div>
         </div>
       </Link>
     </div>
